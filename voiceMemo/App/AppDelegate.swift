fcb49bc3b73293e28ce8191d3ea2150c8cbfe02f
hoit1011 <parksm071011@gmail.com>
2024-08-20 23:30:45 +0900

    Notification

--- voiceMemo/App/AppDelegate.swift
+++ voiceMemo/App/AppDelegate.swift
@@ -5,8 +5,16 @@
 //  Created by 박성민 on 7/25/24.
 //
 
-import Foundation
+import UIKit
 
-class AppDelegate{
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var notificationDelegate = NotificationDelegate()
     
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = notificationDelegate
+        return true
+    }
 }
--- voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -16,7 +16,7 @@ struct OnboardingView: View {
         NavigationStack(path: $pathModel.paths){
 //            OnboardingContentView(onboardingViewModel: onboardingViewModel)
 //            MemoListView()
-            VoiceRecorderView()
+            TimerView()
                 .environmentObject(memoListViewModel)
                 .navigationDestination(
                     for: PathType.self,
